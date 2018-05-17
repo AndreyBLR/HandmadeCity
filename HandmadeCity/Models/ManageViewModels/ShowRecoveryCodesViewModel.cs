417501be79f2e652b0d1ad7d5e7e7caabd39c2b8
Andrey Kukalev <Andrey_Kukalev@epam.com>
2018-05-17 16:54:41 +0300

    Solution is created.

---
+++ HandmadeCity/Models/ManageViewModels/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandmadeCity.Models.ManageViewModels
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
